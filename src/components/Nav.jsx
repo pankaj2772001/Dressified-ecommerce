f52fe57d274192f809a9dc5cedfd27e06e57bce4
Pankaj Gupta <pankaj392pg@gmail.com>
2025-08-25 20:47:32 +0530

    fix: nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -41,94 +41,86 @@ const Nav = () => {
     }
 
     return (
-        <header className="bg-light  pb-1">
-            <nav className="navbar navbar-expand-lg">
+        <header className="bg-light">
+            <nav className="navbar navbar-expand-lg container">
+                <div className="container-fluid">
 
-                <div className="d-flex justify-content-between">
-
-
-                    <div>
-
-                        <Link className="nav-link" to={"/home"}> <h2 className=" text-dark">Dressified</h2> </Link>
-
-                    
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    </div>
-
-                    
-                    
+                     <Link className="nav-link" to={"/home"}> <h2 className=" text-dark mb-0 h1">Dressified</h2> </Link>
 
 
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+
+
+
+        
+                {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button> */}
+
+
+
+
+
+                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+
+                            <div className="input-group">
+                                <span class="input-group-text" ><CiSearch className="fs-5" /></span>
+                                <input className="pe-5 ps-2 py-2 form-control " type="text" onChange={searchSuggestions} value={query} placeholder="What are you looking for?" style={{ width: "15rem" }} />
+                            </div>
+
+
+                            <div>
+                                <ul className={`z-1 position-absolute mt-2 list-group list-group-flush rounded shadow-sm   ${suggestions.length > 0 && "overflow-y-scroll"}`} style={{ maxHeight: "300px" }} >
+
+                                    {suggestions.length > 0 ? suggestions?.map(item => <li className="list-group-item pe-5" style={{ cursor: "pointer" }} onClick={() => {
+                                        navigate(`/section/${item.section}/category/${item.category}`);
+                                        setQuery('')
+                                        setSuggestions([])
+                                    }}>
+                                        <span> {item.category} - <b>in {item.section}</b></span>
+
+
+                                    </li>) : query ? <li className="list-group-item pe-5">No Categories Found</li> : null}
+
+                                </ul>
+                            </div>
+
+
+
+                        </li>
+
+                        <li className="nav-item">
+                            <ul className="navbar-nav gap-4">
+
                                 <li className="nav-item">
-
-                                    <div className="input-group">
-                                        <span class="input-group-text" ><CiSearch className="fs-5" /></span>
-                                        <input className="pe-5 ps-2 py-2 form-control " type="text" onChange={searchSuggestions} value={query} placeholder="What are you looking for?" style={{ width: "15rem" }} />
-                                    </div>
-
-
-                                    <div>
-                                        <ul className={`z-1 position-absolute mt-2 list-group list-group-flush rounded shadow-sm   ${suggestions.length > 0 && "overflow-y-scroll"}`} style={{ maxHeight: "300px" }} >
-
-                                            {suggestions.length > 0 ? suggestions?.map(item => <li className="list-group-item pe-5" style={{ cursor: "pointer" }} onClick={() => {
-                                                navigate(`/section/${item.section}/category/${item.category}`);
-                                                setQuery('')
-                                                setSuggestions([])
-                                            }}>
-                                                <span> {item.category} - <b>in {item.section}</b></span>
-
-
-                                            </li>) : query ? <li className="list-group-item pe-5">No Categories Found</li> : null}
-
-                                        </ul>
-                                    </div>
-
-
-
+                                    <NavLink to={"/user"} className="nav-link position-relative"><FaUser className="fs-4" /></NavLink>
                                 </li>
 
                                 <li className="nav-item">
-                                    <ul className="navbar-nav gap-4">
+                                    <NavLink to={"/wishlist"} className="nav-link position-relative"><FaHeart className="fs-4 " />{wishList.length > 0 && <span class="position-absolute top-1 ms-2 translate-middle badge rounded-pill bg-primary">{wishList.length}
+                                    </span>}
 
-                                        <li className="nav-item">
-                                            <NavLink to={"/user"} className="nav-link position-relative"><FaUser className="fs-4" /></NavLink>
-                                        </li>
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item ">
+                                    <NavLink to={"/cart"} className="nav-link  position-relative"><FaShoppingCart className="fs-4" />{cart.item?.length > 0 && <span class="position-absolute top-1 ms-2 translate-middle badge rounded-pill bg-primary">{cart.item?.length}
 
-                                        <li className="nav-item">
-                                            <NavLink to={"/wishlist"} className="nav-link position-relative"><FaHeart className="fs-4 " />{wishList.length > 0 && <span class="position-absolute top-1 ms-2 translate-middle badge rounded-pill bg-primary">{wishList.length}
-                                            </span>}
-
-                                            </NavLink>
-                                        </li>
-                                        <li className="nav-item ">
-                                            <NavLink to={"/cart"} className="nav-link  position-relative"><FaShoppingCart className="fs-4" />{cart.item?.length > 0 && <span class="position-absolute top-1 ms-2 translate-middle badge rounded-pill bg-primary">{cart.item?.length}
-
-                                            </span>}</NavLink>
-                                        </li>
-
-                                    </ul>
+                                    </span>}</NavLink>
                                 </li>
 
                             </ul>
+                        </li>
 
-
-
-
-                        </div>
-
-
-
-
-
-
-
-                    
+                    </ul>
 
                 </div>
+                {/* </div> */}
+
+
+
+
+               
 
             </nav>
 
